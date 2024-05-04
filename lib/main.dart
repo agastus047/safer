f0523f6e0510b3299a9595e5f9a0c5f621d20cb9
pineapple <sajithappu1804@gmail.com>
2024-05-04 19:21:29 +0530

    splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:safer/page2.dart';
+import 'package:safer/splash.dart';
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: 'page2',
-    routes: {
+    initialRoute: 'splash',
+    routes: {'splash': (BuildContext) => SplashScreen(),
       'page2': (BuildContext) => MyPage2(),
     },
-  ));
+  )
+  );
 }
