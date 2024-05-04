55c2b882df9ca8f11c8af3b5a57c6fd54d7d5a69
WavBoi <emmanueljose003@gmail.com>
2024-05-05 03:05:34 +0530

    map working

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,20 @@ import 'package:safer/login.dart';
 import 'package:safer/signup.dart';
 import 'package:safer/form.dart';
 import 'package:safer/page2.dart';
+import 'package:safer/map_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: 'login',
+    initialRoute: 'map',
     routes: {
       'login': (BuildContext) => LoginPage(),
       'sign': (BuildContext) => SignUp(),
       'form': (BuildContext) => FormDemo(),
       'page2': (BuildContext) => MyPage2(),
+      'map': (BuildContext) => MapPage(),
     },
   ));
 }
