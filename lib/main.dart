847cc3b4bc1ff221ce0b1584d74176af23ea1f51
Krishnaraju06 <krishnapriyaraju07@gmail.com>
2024-05-05 02:02:39 +0530

    s

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:safer/firebase_options.dart';
 import 'package:safer/login.dart';
 import 'package:safer/signup.dart';
 import 'package:safer/form.dart';
 import 'package:safer/page2.dart';
 import 'package:safer/splash.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: 'splash',
@@ -20,6 +22,5 @@ void main() {
       'form': (BuildContext) => FormDemo(),
       'page2': (BuildContext) => MyPage2(),
     },
-  )
-  );
+  ));
 }
