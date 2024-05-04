9a5765943fd3a403b5b9acd267b95a49f40697f1
WavBoi <emmanueljose003@gmail.com>
2024-05-04 19:19:48 +0530

    added map page

---
+++ lib/map_page.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps'
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  @override
+  Widget build(buildContext context) {
+    return Scaffold(
+      body: ());
+  }
+}
